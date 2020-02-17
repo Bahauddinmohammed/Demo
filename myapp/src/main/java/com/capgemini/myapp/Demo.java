802f9a280b09ccab0abcdf244571696b14a84fc8
BVRIT <BVRIT@ECE-LAB-213>
2020-02-17 12:52:49 +0530

    Demo project

---
+++ myapp/src/main/java/com/capgemini/myapp/Demo.java
@@ -0,0 +1,14 @@
+package com.capgemini.myapp;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	
+	
+	int add(int a, int b) {
+	
+		  Scanner sc = new Scanner(System.in);
+	
+}
+}
---
+++ myapp/src/main/java/com/capgemini/myapp/DemoTest.java
@@ -0,0 +1,21 @@
+package com.capgemini.myapp;
+
+import junit.framework.TestCase;
+
+public class DemoTest extends TestCase {
+	static Demo test =new Demo();
+@Test
+   public static void testAdd() {
+	   assertEquals(10,test.add(5,5));
+   }
+	   
+	   //public static void main(String[] args) {
+	
+	
+	
+	  
+	   
+			   
+	   
+   }
+
